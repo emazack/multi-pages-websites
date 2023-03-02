3be3e625326aac16645ff5d44a388d36df7a4920
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-02 12:44:24 +0100

    Init struncture folders

---
+++ src/pages/locations/Locations.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+        <>
+            <div>Hello World</div>
+            <div className="sass">
+                <div className="name">
+                    Hello sass in vite
+                </div> 
+            </div>
+        </>
+    );
+}
